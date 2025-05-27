10f0e26f4f98c600927761ae930ff6d1dbeaaacf
hyewon4052 <s2311@e-mirim.hs.kr>
2025-05-27 14:45:58 +0900

    고전적인 스타일로 Box 클래스 정의

---
+++ propExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box(100, 200);
+            Console.WriteLine(box1.Area());
+        }
+    }
+}
