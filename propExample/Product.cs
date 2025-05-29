831874a80e7a984381a4e7e81ec48a6632ccaa3e
hyewon4052 <s2311@e-mirim.hs.kr>
2025-05-29 09:43:32 +0900

    소멸자 생성

--- propExample/Product.cs
+++ propExample/Product.cs
@@ -10,11 +10,22 @@ namespace propExample
     {
         public String name;
         public int price;
+        static Product product;
+
+        static Product()
+        {
+            product = null;
+            Console.WriteLine("정적 생성자 호출");
+        }
+        ~Product() {
+            Console.WriteLine("Product 객체 소멸자 생성!");
+        }
         private Product(String name,int price)
         {  
             this.name = name;
             this.price = price;
         }
+
         public static Product GetInstance(string name, int price) { 
             if (product == null)
             {
