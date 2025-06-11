a7be9c3c1622dcf6e2f4cbcd2aa6fccc94e1829e
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-11 13:59:48 +0900

    강아지 고양이 클래스

---
+++ PolyExample32/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyExample32
+{
+    class Dog
+    {
+        public Dog()
+        {
+            Age = 0;
+        }
+
+        public int Age { get; set; }
+        public int Color { get; set; }
+
+        public void Eat() { Console.WriteLine("냠냠 먹습니다.");  }
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); } 
+        public void Bark() { Console.WriteLine("왈왈 짖습니다..");  }
+    }
+}
---
+++ PolyExample32/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyExample32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Dog> dogs= new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            foreach (var item in dogs) {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+
+            foreach (var item in cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+        }
+    }
+}
