f2f1d07c628660e0393b5d54660dce93b3c56e40
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-12 14:05:31 +0900

    상속 시 생성자 예

---
+++ ClassExample32/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample32
+{
+    internal class Program
+    {
+        class Parent
+        {
+            public Parent()
+            {
+                Console.WriteLine("Parent() : ");
+            }
+            public Parent(int n)
+            {
+                Console.WriteLine("Parent(int param)");
+            }
+            public Parent(string name)
+            {
+                Console.WriteLine("Parent(string param) ");
+            }
+        }
+        class Child : Parent
+        {
+            public Child() : base(10)
+            {
+                Console.WriteLine("Child() : base(10)");
+            }
+            public Child(string input) : base(input)
+            {
+                Console.WriteLine("Child (string input) : base(input)");
+            }
+            static void Main(string[] args)
+            {
+                Child childA = new Child();
+                Child childB = new Child("string");
+            }
+        }
+    }
+}
