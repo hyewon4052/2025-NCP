d43b5c48ef6dcd6b9491a0e2744292d28ecb54f1
hyewon4052 <s2311@e-mirim.hs.kr>
2025-08-21 16:04:11 +0900

    Reapply "#1 사용자 입력 구현"
    This reverts commit 2cdd2ae32088edd3b5a5b7f56e7df675a3f838ff.

--- FortuneTeller/Form1.cs
+++ FortuneTeller/Form1.cs
@@ -16,5 +16,25 @@ namespace FortuneTeller
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
